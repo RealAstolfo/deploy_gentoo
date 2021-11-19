f5846298d6f10f908560d2edae220f558dac45bd
Astolfo <Astolfo@gmx.com>
2021-11-18 22:54:46 -0600

    update deploy script

--- source/deploy_gentoo.sh
+++ source/deploy_gentoo.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 emerge --sync
 emerge --update --deep --newuse @world
-emerge layman cpuid2cpuflags
+emerge layman cpuid2cpuflags gentoo-sources
 layman -a guru
 layman -a steam-overlay
 
+cp config/settings.ini /etc/gtk-3.0/
 cp -r ./source/portage/* /etc/portage/
 cd ./source/suckless
 cp patches/default-256.lua /usr/share/vis/themes
 sh -c "patch dwm/config.h patches/dwm_patch && cd dwm && make install"
 sh -c "patch slstatus/config.h patches/slstatus_patch && cd slstatus && make install"
 sh -c "cd st && make install"
+
+emerge nvidia-drivers
+emerge --ask --update --deep --changed-use @world
